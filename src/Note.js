d3ac00b8816d1dc9b4cc394d8e30c1dbcfc0bccf
sunisakc <sunisa.k@gmail.com>
2018-11-14 10:47:33 +0100

    Added edit

--- src/Note.js
+++ src/Note.js
@@ -1,19 +1,64 @@
 import React, { Component } from 'react'
 import { FaPencilAlt } from 'react-icons/fa'
 import { FaTrash } from 'react-icons/fa'
+import {  FaFileAlt } from 'react-icons/fa'
 
 
 class Note extends Component {
-    render() {
+    constructor(props) {
+        super(props)
+        this.state = {
+            editing:false
+        }
+        this.edit = this.edit.bind(this)
+        this.remove = this.remove.bind(this)
+        this.save = this.save.bind(this)
+        this.renderForm = this.renderForm.bind(this)
+        this.renderDisplay = this.renderDisplay.bind(this)
+    }
+    edit() {
+        this.setState({
+            editing:true
+        })
+    }
+    remove() {
+        alert('removing note')
+    }
+
+    save() {
+        alert('save')
+    }
+
+    renderForm() {
+        return (
+            <div className="note">
+                <form>
+                    <textarea />
+                    <button onClick={this.save}><  FaFileAlt /></button>
+                </form>
+            
+            </div>
+        )
+    }
+
+
+    renderDisplay() {
         return (
             <div className="note">
              <p>Learn to Code</p>
               <span>
-                    <button>< FaPencilAlt /></button>
-                    <button><FaTrash /></button>
+                    <button onClick={this.edit} id="edit">< FaPencilAlt /></button>
+                    <button onClick={this.remove}id="remove" ><FaTrash /></button>
               </span>
             </div>
         )
+    }
+    render() {
+        if(this.state.editing){
+            return this.renderForm()
+        }else {
+            return this.renderDisplay()
+        }
     }
 }
 
